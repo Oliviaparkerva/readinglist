26b90937841cdb5d992e9e55a5dde107b0af3460
Olivia Parker <oliviaparkerva@gmail.com>
2019-05-13 00:11:21 -0400

    running local host

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import MainPage from './Components/MainPage';
 import SearchPage from './Components/SearchPage';
 import * as BooksAPI from './BooksAPI';
 
+const http = require('http');
+const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
+});
+server.listen(port,() => {
+console.log(`Server running at port `+port);
+});
+
 class BooksApp extends React.Component {
   
   state={
